62dbca32f5728087e0334b90ae0a1a7e87c7bc2c
Paul Sonkoly <sonkoly.pal@gmail.com>
2020-01-26 12:22:16 +0000

    Added indices

--- config/schema.rb
+++ config/schema.rb
@@ -15,7 +15,7 @@ end
 
 create_table :moves do
   primary_key :id, type: 'bigint'
-  foreign_key :game_id, :games, type: 'bigint', null: false
+  foreign_key :game_id, :games, type: 'bigint', null: false, on_delete: :cascade
   String :fen_position, size: 72, null: false
   String :san, size: 10, null: false
   String :next_san, size: 10
@@ -24,4 +24,15 @@ create_table :moves do
   Fixnum :castling_availability, type: 'smallint', null: false
   Fixnum :halfmove_clock, type: 'smallint', null: false
   Fixnum :en_passant, type: 'smallint'
+
+  index :castling_availability, name: 'ix_moves_on_castling_availability',
+                                type: 'btree'
+  index :en_passant, name: 'ix_moves_on_en_passant', type: 'btree'
+  index :fen_position, name: 'ix_moves_on_fen_position', type: 'btree'
+  index :fen_position, name: 'ix_moves_on_fen_position_hash', type: 'hash'
+  index :fullmove_number, name: 'ix_moves_on_fullmove_number', type: 'btree'
+  index :game_id, name: 'ix_moves_on_game_id', type: 'btree'
+  index :active_colour, name: 'ix_moves_on_active_colour', type: 'btree'
 end
+
+run 'CLUSTER "moves" USING "ix_moves_on_fen_position"'
